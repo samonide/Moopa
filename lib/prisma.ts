aef1e90f15ac0b80fa7f1b54131337f4ebeed797
samonide <itzsamonide@gmail.com>
2025-12-25 19:25:32 +0600

    update

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -4,6 +4,13 @@ declare global {
   var prisma: PrismaClient | undefined;
 }
 
-export const prisma = global.prisma || new PrismaClient();
+export const prisma = global.prisma || new PrismaClient({
+  log: process.env.NODE_ENV === "development" ? ["query", "error", "warn"] : ["error"],
+  datasources: {
+    db: {
+      url: process.env.DATABASE_URL,
+    },
+  },
+});
 
 if (process.env.NODE_ENV !== "production") global.prisma = prisma;
